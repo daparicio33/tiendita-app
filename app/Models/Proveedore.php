71a886ea5850b6e413356f30a0d06ba37993b73a
0elizabeth <ferchipinzonh@gmail.com>
2023-01-20 10:54:56 -0500

    modelos y migraciones

---
+++ app/Models/Proveedore.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Proveedore extends Model
+{
+    use HasFactory;
+    public function compra(){
+        return $this->hasMany(Compra::class);
+    }
+}
---
+++ database/migrations/2023_01_20_142211_create_cdetalles_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCdetallesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('cdetalles', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('compra_id');
+            $table->foreign('compra_id')->references('id')->on('compras');
+            $table->unsignedBigInteger('catalogo_id');
+            $table->foreign('catalogo_id')->references('id')->on('catalogos');
+            $table->decimal('cantidad',4, 0);
+            $table->decimal('precio', 3, 2);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('cdetalles');
+    }
+}
